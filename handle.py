3cac68c50e0495f0ceb968974f619faab664ecaf
Keshav Agrawal <keshav_agrawal89@hotmail.com>
2013-08-03 06:28:34 +0530

    SMS Dashboard

--- handle.py
+++ handle.py
@@ -8,21 +8,20 @@ import os
 from os.path import dirname, join as joinpath
 import redis
 import requests
-
+import uuid
+import time
 
 app = Flask(__name__)
 
 #redis = redis.Redis('localhost')
 
-# This is for heroku redistogo addon #
-redis_url = os.getenv('REDISTOGO_URL','redis://localhost:<port>')
+redis_url = os.getenv('REDISTOGO_URL','redis://localhost:9444')
 redis = redis.from_url(redis_url)
-#####################################
 
 gl_autoreply = "Default reply"
 gl_voice_recording = ""
 
-auth_id = '<auth_id>'
+auth_id = '<auth_id>
 auth_token = '<auth_token>'
 
 
@@ -33,7 +32,7 @@ def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'ryan' and password == 'password'
+    return username == 'username' and password == 'password'
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
@@ -65,11 +64,52 @@ def accept_and_reply():
 	src = request.form.get('From','')
 	plivo_did = request.form.get('To','')
 
+	time_sms_received = time.asctime(time.localtime(time.time()))
+	sms_key = "sms_%s" % uuid.uuid4()
+	
+	redis.hset(sms_key,"From", src)
+	redis.hset(sms_key,"Plivo DID", plivo_did)
+	redis.hset(sms_key,"Text", text)
+	redis.hset(sms_key,"Time Received", time_sms_received)
+	redis.hset(sms_key,"Replied",'n')
+
 	send_auto_response(src,plivo_did,text)
 
 	email_SMS(text,src,plivo_did)
 
 	response = make_response("OK")
+	response.headers['Content-type'] = 'text/html'
+	return response
+
+
+@app.route('/sms-dashboard/', methods=['GET','POST'])
+def prepare_sms_object():
+
+	no_of_sms = len(redis.keys('sms_*'))
+	sms_keys = redis.keys('sms_*')
+	sms_text = []
+	sms_time = []
+	sms_from = []
+	sms_to = []
+	sms_key = []
+	sms_replied = []
+	
+	
+
+	for key in sms_keys:
+		sms_replied.append(redis.hget(key,'Replied'))
+		sms_text.append(redis.hget(key,'Text'))
+		sms_from.append(redis.hget(key,'From'))
+		sms_to.append(redis.hget(key,'Plivo DID'))
+		sms_time.append(redis.hget(key,'Time Received'))
+		sms_key.append(key)
+		
+
+	sms_object = {'sms_from':sms_from, 'sms_to':sms_to, 'sms_text':sms_text, 'sms_time':sms_time, 'sms_key':sms_key, \
+					'sms_replied':sms_replied}
+	
+	response = make_response(render_template("sms_dashboard.html", no_of_sms = no_of_sms-1,\
+				sms_object = sms_object))
 	response.headers['Content-type'] = 'text/html'
 	return response
 
@@ -111,7 +151,7 @@ def set_email_config():
 
 def email_SMS(text, src, plivo_did):
 	SECRET_KEY = redis.get('mailgun_token')
-	from_email = "Ryan %s" % redis.get('from_email')
+	from_email = "UserXYZ %s" % redis.get('from_email')
 	to_email = redis.get('to_email')
 	subject = redis.get('mail_subject')
 	domain = redis.get('domain')
@@ -129,11 +169,27 @@ def email_SMS(text, src, plivo_did):
 	return send_mail_response
 
 
-def send_auto_response(src, plivo_did, text):
+def send_auto_response(src, plivo_did, text, ui_flag='False'):
 	sms_auto_response = get_sms_response()
-	response = cloud.send_message({'src':plivo_did,'dst':src, 'text':sms_auto_response})
-	print response
+	if ui_flag == 'True':
+		response = cloud.send_message({'src':plivo_did,'dst':src, 'text':text})
+	else:
+		response = cloud.send_message({'src':plivo_did,'dst':src, 'text':sms_auto_response})
+	return response
 
+@app.route('/reply/', methods=['POST'])
+def send_ui_reply():
+	sms_id = request.form.get('sms_id')
+	sms_reply = request.form.get('reply')
+
+	print "Here I am: SMS Id - %s" % sms_id
+
+	if sms_reply != "":
+		src = redis.hget(sms_id,'From')
+		plivo_did = redis.hget(sms_id,'Plivo DID')
+		redis.hset(sms_id,"Replied",'y')
+		send_auto_response(src, plivo_did, sms_reply, 'True')
+	return "OK"
 
 
 @app.route('/set_recording_voice/', methods=['POST'])
@@ -159,7 +215,7 @@ def acceptcalls():
 def get_play_url():
 	return redis.get('play_url')
 
-@app.route('/help/', methods=['POST'])
+@app.route('/help/', methods=['GET','POST'])
 def help():
 	response = make_response(render_template("help.html"))
 	response.headers['Content-type'] = 'text/html'
